a8c168749bda1d397ac1c61d89ec96da0a7d3a1d
koheitakahashi <t.NMP300@gmail.com>
2020-06-12 14:34:31 +0900

    楽曲のYouTubeのURLを表示する機能に関連するテストを追加

--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "support/webmock_spotify_api_helpers.rb"
+# TODO API helperが多いので、それをまとめるhelperを作成すること
+require "support/webmock_spotify_api_helper.rb"
+require "support/webmock_youtube_api_helper.rb"
 
 describe "楽曲検索機能", type: :system do
-  include WebmockSpotifyApiHelpers
+  include WebmockSpotifyApiHelper
+  include WebmockYoutubeApiHelper
 
   context "検索フォームに入力した楽曲をSpotify APIを介して検索" do
     before do
-      mock_search_results
+      mock_spotify_search_results
+      mock_youtube_search_results
     end
 
     it "検索結果を表示する" do
@@ -16,6 +20,14 @@ describe "楽曲検索機能", type: :system do
       fill_in "query", with: "リライト"
       click_on "Search"
       expect(page).to have_content "ASIAN KUNG-FU GENERATION"
+    end
+
+    it "SpotifyとYoutubeのURLを表示する" do
+      visit root_path
+      fill_in "query", with: "リライト"
+      click_on "Search"
+      expect(page).to have_selector '.spotify-url', text: 'https://open.spotify.com/track/3h5e4tpgR9q0cjQXzo8FMD?si=WN7JPlJ5SuSQhrhh9Z-8Yw'
+      expect(page).to have_selector '.youtube-url', text: "https://youtu.be/cr8magEp0Ho"
     end
   end
 end
