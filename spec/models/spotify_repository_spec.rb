9ffba6f7d70f0bea3432efbe665b2637a8a86ac0
koheitakahashi <t.NMP300@gmail.com>
2020-06-17 14:10:53 +0900

    spotify_repository_spec.rbのテストを修正
    expectedを修正した

--- spec/models/spotify_repository_spec.rb
+++ spec/models/spotify_repository_spec.rb
@@ -3,7 +3,7 @@
 require "spec_helper"
 require "rails_helper"
 require "webmock/rspec"
-require "support/webmock_spotify_api_helper.rb"
+require "support/webmock_spotify_api_helper"
 
 describe "SpotifyRepositoryのmodelテスト", type: :model do
   include WebmockSpotifyApiHelper
@@ -14,9 +14,16 @@ describe "SpotifyRepositoryのmodelテスト", type: :model do
 
     it "#search" do
       spotify = SpotifyRepository.new
-      actual = spotify.search("リライト").first[:spotify_title]
+      actual = spotify.search("リライト")
+      expected = [
+        {
+          spotify_title: "リライト",
+          spotify_artists: "ASIAN KUNG-FU GENERATION",
+          spotify_url: "https://open.spotify.com/track/3h5e4tpgR9q0cjQXzo8FMD?si=WN7JPlJ5SuSQhrhh9Z-8Yw"
+        }
+      ]
 
-      expect("リライト").to eq actual
+      expect(expected).to eq actual
     end
   end
 end
