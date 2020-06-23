b8b014726103f22a50ee04b5a8e34674ea94b26e
koheitakahashi <t.NMP300@gmail.com>
2020-06-23 15:08:21 +0900

    楽曲情報を統合する処理をモデルに追加したことにより、コントローラーを修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,26 +6,11 @@ class SearchesController < ApplicationController
 
   def show
     # TODO 検索フォームが空の時にエラーを表示するなどの処理を加えること
-    query = params[:query]
-    return unless query.present?
+    keyword = params[:keyword]
+    return unless keyword.present?
 
     # TODO 複数のAPIからのレスポンスを曲名、アーティス名を参照して統合して表示できるようにすること
-    spotify = SpotifyRepository.new
-    youtube = YoutubeRepository.new
-    apple_music = AppleMusicRepository.new
-    spotifay_response = spotify.search(query)
-    youtube_response = youtube.search(query)
-    apple_music_response = apple_music.search(query)
-    @results = sounds_data(spotifay_response, youtube_response, apple_music_response)
+    sounds_api = SoundApisRepository.new
+    @results = sounds_api.search(keyword)
   end
-
-  private
-    # TODO mapを使った処理ができないかを考えること
-    def sounds_data(spotifay_response, youtube_response, apple_music_response)
-      data = []
-      spotifay_response.each_with_index do |s_res, index|
-        data << s_res.merge(youtube_response[index], apple_music_response[index])
-      end
-      data
-    end
 end
