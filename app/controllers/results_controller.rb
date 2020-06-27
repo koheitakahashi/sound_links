63bd2504bf7543d08a206890067fc79224e03397
koheitakahashi <t.NMP300@gmail.com>
2020-06-27 12:46:26 +0900

    searchとresultsのルーティングを変更したため、controllerを切り分けた
    searchとresultsのリソースを分けたため、合わせてcontrollerも分けた。

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
-class SearchesController < ApplicationController
+class ResultsController < ApplicationController
   def index
-  end
-
-  def show
     # TODO 検索フォームが空の時にエラーを表示するなどの処理を加えること
     keyword = params[:keyword]
     return unless keyword.present?
 
-    # TODO 複数のAPIからのレスポンスを曲名、アーティス名を参照して統合して表示できるようにすること
     sounds_api = SoundApisRepository.new
     @results = sounds_api.search(keyword)
   end
