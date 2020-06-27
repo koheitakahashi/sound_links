dd5f9dc57da78a6afd38506549b035643dce6453
koheitakahashi <t.NMP300@gmail.com>
2020-06-27 12:45:04 +0900

    searchとresultsのルーティングを変更
    searchのindexとshowでリソースを表現するのは不適切だと考えられたため、search#indexとresults#idexでリソースを表現した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: "searches#index"
-  resource "searches", only: %i(index show)
+  root to: "search#index"
+
+  get "results", to: "results#index"
 end
