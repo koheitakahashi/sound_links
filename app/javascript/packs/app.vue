84b7a90017df0ca12c8bf83a8f7a2ad344d9c2e4
koheitakahashi <koheitakahashi26@gmail.com>
2020-12-26 17:26:52 +0900

    楽曲の URL をコピーする機能を追加

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -22,15 +22,23 @@
             <th>Spotify</th>
             <th>Apple Music</th>
             <th>KKBOX</th>
+            <th></th>
           </tr>
         </thead>
-        <tbody v-for="result in state.results">
+        <tbody v-for="result in state.results" :key="result.isrc">
           <tr>
             <td>{{ result.title }}</td>
-            <td>{{ result.artist}}</td>
-            <td>{{ result.spotifyUrl }} </td>
+            <td>{{ result.artist }}</td>
+            <td>{{ result.spotifyUrl }}</td>
             <td>{{ result.appleMusicUrl }}</td>
             <td>{{ result.kkboxUrl }}</td>
+            <td>
+              <button @click="copyUrlsToClipBoard(result)">
+                この曲をシェアする
+              </button>
+              <div v-show="state.isCopiedUrls">クリップボードにURLがコピーされました</div>
+              <div v-show="state.isFailedUrls">URLのコピーが失敗しました</div>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -43,12 +51,16 @@ import { defineComponent, reactive } from "vue";
 import axios from "axios";
 import { parseResponseData } from "./utils/parseResponseData";
 
+const TOOLTIP_SHOW_TIME = 3000;
+
 export default defineComponent({
   name: "App",
   setup() {
     const state = reactive({
       keywords: "",
       results: [],
+      isCopiedUrls: false,
+      isFailedUrls: false,
     });
 
     async function submitSearch() {
@@ -58,13 +70,48 @@ export default defineComponent({
         });
         state.results = parseResponseData(response.data);
       } catch (error) {
+        // TODO: エラー時の処理を追加
         console.log(`Error! : ${error}`);
       }
+    }
+
+    function copyUrlsToClipBoard(result) {
+      navigator.clipboard
+        .writeText(urlsText(result))
+        .then(() => {
+          state.isCopiedUrls = true;
+          setTimeout(function () {
+            state.isCopiedUrls = false;
+          }, TOOLTIP_SHOW_TIME);
+        })
+        .catch(() => {
+          state.isFailedCopyUrls = true;
+          setTimeout(function () {
+            state.isFailedCopyUrls = false;
+          }, TOOLTIP_SHOW_TIME);
+        });
+    }
+
+    function urlsText(result) {
+      let urls = [`${result.title}(${result.artist})`];
+
+      if (result.spotifyUrl) {
+        urls.push(`Spotify\n${result.spotifyUrl}`);
+      }
+      if (result.appleMusicUrl) {
+        urls.push(`Apple Music\n${result.appleMusicUrl}`);
+      }
+      if (result.kkboxUrl) {
+        urls.push(`KKBOX\n${result.kkboxUrl}`);
+      }
+
+      return urls.join("\n");
     }
 
     return {
       state,
       submitSearch,
+      copyUrlsToClipBoard,
     };
   },
 });
