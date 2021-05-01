544166c6b26ca32572e4a4dff170222a46931bef
koheitakahashi <koheitakahashi26@gmail.com>
2021-05-01 14:28:12 +0900

    フロントエンド側で楽曲名とアーティスト名の文字数を制限して表示させるようにした

---
+++ app/javascript/src/utils/truncateText.ts
@@ -0,0 +1,8 @@
+const MAX_STRING_LENGTH = 30
+
+export function truncateText (string: string) :string {
+  if (string.length <= MAX_STRING_LENGTH) {
+    return string
+  }
+  return string.slice(0, MAX_STRING_LENGTH) + "..."
+}
