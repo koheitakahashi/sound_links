bf2846b2ee169cde33f7c4657ec3636bfd045017
koheitakahashi <koheitakahashi26@gmail.com>
2021-05-03 16:57:50 +0900

    テストヘルパーのコーディングスタイルを修正した

--- spec/support/webmock_apple_music_helper.rb
+++ spec/support/webmock_apple_music_helper.rb
@@ -6,14 +6,12 @@ module WebmockAppleMusicHelper
   def mock_apple_music_search_results
     WebMock.stub_request(:get, "https://api.music.apple.com/v1/catalog/jp/search?limit=5&term=リライト&types=songs")
            .to_return(status: 200,
-                      body: File.read("#{Rails.root}/spec/fixtures/apple_music_api_response.json"),
-                      headers: { "Content-Type" =>  "application/json" }
-      )
+                      body: file_fixture("apple_music_api_response.json"),
+                      headers: { "Content-Type" =>  "application/json" })
   end
 
   def mock_apple_music_error_response
     WebMock.stub_request(:get, "https://api.music.apple.com/v1/catalog/jp/search?limit=5&term=bad_params&types=songs")
-           .to_return(status: 401,
-                      body: File.read("#{Rails.root}/spec/fixtures/apple_music_api_error_response.json"))
+           .to_return(status: 401, body: file_fixture("apple_music_api_error_response.json"))
   end
 end
