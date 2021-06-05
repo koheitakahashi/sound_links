ef95c8433958aa3c405945edba0a646761bb1040
koheitakahashi <koheitakahashi26@gmail.com>
2021-06-05 21:13:13 +0900

    $ bin/rails app:update

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format. Inflections
@@ -13,6 +11,6 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.acronym "API"
-end
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
