f4a6b2578cbd2d3664e5de5115abc40232bd218d
koheitakahashi <kouhei.takahashi@everyleaf.com>
2021-07-17 11:46:15 +0900

    [API]検索結果のレスポンスを整形した
    - キー名を camelCase に変換
    - エラーレスポンスを JSON 形式になるように設定を追加
    - routes を json しか受け付けないように設定を追加

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -72,4 +72,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # not default
+  config.debug_exception_response_format = :api
 end
