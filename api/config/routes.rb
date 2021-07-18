249822b4fce4d20efc181fbcfdbf7e28a2446658
koheitakahashi <kouhei.takahashi@everyleaf.com>
2021-07-18 15:22:04 +0900

    [API]$ be rubocop -A

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace "api", { format: 'json' } do
+  namespace "api", { format: "json" } do
     namespace "v1" do
       get "search", to: "searches#index"
     end
