59a04b727a55f49b438f27c3faf4c744f6522a78
koheitakahashi <kouhei.takahashi@everyleaf.com>
2021-09-20 12:13:12 +0900

    [frontend]エラーページのデザインを更新

--- frontend/src/components/ErrorMessage.vue
+++ frontend/src/components/ErrorMessage.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="error-message__content">
-    <exclamation-circle-icon size="5x" class="error-icon"></exclamation-circle-icon>
-    <div class="error-message__text">
-      <p>エラーにより検索結果を表示することができませんでした。</p>
-      <p>別のキーワードで検索いただくか、時間をおいてからお試しください。</p>
+  <div class="error-message-layout">
+    <div class="error-message__content">
+      <div class="error-message__header">
+        <img src="/img/icons/exclamation-icon.svg" alt="エクスクラメーションマークのアイコン">
+        <h2>何らかの問題が起きました</h2>
+      </div>
+      <div class="error-message__text">
+        <p>ご迷惑おかけして、申し訳有りません。</p>
+        <p>問題が起きてページが表示できませんでした。</p>
+        <p>別のキーワードで検索いただくか、時間をおいてからお試しください。</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import ExclamationCircleIcon from './fontAwesome/ExclamationCircleIcon.vue';
 
 export default defineComponent({
   name: 'ErrorMessage',
-  components: { ExclamationCircleIcon },
 });
 </script>
 
