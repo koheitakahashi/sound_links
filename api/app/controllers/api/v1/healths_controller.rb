edfa1aec1bab47650688f3d70bdb916f7b940abe
koheitakahashi <kouhei.takahashi@everyleaf.com>
2021-09-26 15:31:05 +0900

    [api]AWS ELB のヘルスチェックが失敗していたため修正
    Searches#indexで独自ヘッダがない場合は401を返していたため、ヘルスチェックが失敗していた。
    そのため、ヘルスチェック用のリソースを作成した。

---
+++ api/app/controllers/api/v1/healths_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module API
+  module V1
+    # NOTE: AWS ELB のヘルスチェック用の controller。Searches#indexは独自ヘッダをリクエストに含めないと401を返すため、ヘルスチェックが失敗する。
+    #   ゆえに、ヘルスチェック用のcontrollerを作成。
+    class HealthsController < ApplicationController
+      def index
+        render json: { message: "Health check is successfully" }, status: :ok
+      end
+    end
+  end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace "api", { format: "json" } do
     namespace "v1" do
       get "search", to: "searches#index"
+      get "health", to: "healths#index"
     end
   end
 end
